e42d7f1f4f3d878aef0d22158e9c2f08f2df5962
raphael56 <raphael.hervieux@gmail.com>
2022-02-10 16:19:27 +0100

    maj react

---
+++ vignettealpha-app/vignette_alpha/accueil.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/style.css">
+    <title>Vignette Alpha</title>
+</head>
+<body>
+    <nav class="container-nav">
+        <li ><a class="box-img" href="#"><img src="ressources/commander.png" alt=""></a></li>
+        <li ><a class="box-img" href="#"><img src="ressources/instruire icone.png" alt=""></a></li>
+        <li ><a class="box-img" href="#"><img src="ressources/se situer.png" alt=""></a></li>
+        <li ><a class="box-img" href="#"><img src="ressources/eduquer.png" alt=""></a></li>
+        <li ><a class="box-img" href="#"><img src="ressources/communiquer.png" alt=""></a></li>
+    </nav>
+<section class="nav-gauche">
+    <div class="box-info info-1">info 1</div>
+    <div class="box-info info-2">info 2</div>
+    <div class="box-info info-3">info 3</div>
+    <div class="box-info info-4">info 4</div>
+    <div class="box-info info-5">info 5</div>
+</section>
+
+    <section class="container-info">
+
+    <div class="container-video"><video controls src="ressources/6km6o-zf0bq.mp4" type="video/mp4"></video></div>
+
+
+        <div class="container-form">
+            
+    <?php 
+    include("formulaire.php"); 
+  ?>
+
+            
+        </div>
+    
+        
+    </section>
+
+</body>
+</html>
